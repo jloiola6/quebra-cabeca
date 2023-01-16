dca5741d869fb97661c25ca72d39a7d7a0a74003
João Teixeira <jloiola6@outlook.com>
2023-01-16 13:28:01 -0500

    Nova estruturação do código

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-pecas = []
+class QuebraCabeca():
 
-def cada_peca(roi, x, y, w, h):
-    try:
-        inicio_coluna = x 
-        fim_coluna = x + w 
-        
-        inicio_linha = y
-        fim_linha = y + h 
+    def __init__(self):
+        self.pecas_anterior = 0
+        self.cap = cv2.VideoCapture(0)
 
-        peca = roi[inicio_linha : fim_linha, inicio_coluna : fim_coluna]
-        large_image = cv2.imread('base.png')
+        # objetos  detectados pela camera
+        self.object = cv2.createBackgroundSubtractorMOG2()
 
-        result = cv2.matchTemplate(peca, large_image, cv2.TM_CCORR)   
+    
+    def exibir_resultado(self, titulo, frame):
+        cv2.imshow(titulo, frame)
+
+    
+    def comparador_imagens(self, peca):
+        self.large_image = cv2.imread('media/base.png')
+        result = cv2.matchTemplate(peca, self.large_image, cv2.TM_SQDIFF_NORMED)   
 
         # We want the minimum squared difference
         mn,_,mnLoc,_ = cv2.minMaxLoc(result)
@@ -30,67 +33,81 @@ def cada_peca(roi, x, y, w, h):
         # Step 3: Draw the rectangle on large_image
         # MPx -= 22
         # MPy += 22
-        cv2.rectangle(large_image, (MPx-tcols,MPy-trows),(MPx+tcols,MPy+trows),(0,0,255),2)
+        cv2.rectangle(self.large_image, (MPx-tcols,MPy-trows),(MPx+tcols,MPy+trows),(0,0,255),2)
 
         # Display the original image with the rectangle around the match.
-        cv2.imshow('output',large_image)
+        self.exibir_resultado('output', self.large_image)
 
         # # The image is only displayed if we call this
         # cv2.waitKey(0)
 
-        # cv2.imwrite(filename=f'pecas/a.jpg', img=peca)
-        # cv2.imshow('Peça', peca)
-    except:
-        pass
 
-# Criar um Tracker
-# tracker = EuclideanDistTracker()
-
-cap = cv2.VideoCapture(1)
-
-# objetos  detectados pela camera
-object = cv2.createBackgroundSubtractorMOG2()
-
-pecas_anterior = 0
-while True:
-    qtd_pecas = 0
-
-    ret, frame = cap.read()
-
-    # Extraindo região importantes
-    roi = frame[10:600, 10:600]
-
-    mask = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-    _, threshold = cv2.threshold(mask, 40, 255, cv2.THRESH_BINARY)
-    # _, threshold = cv2.threshold(mask, 255, 40, cv2.THRESH_BINARY)
-    
-    contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-
-        # Calculando area e removendo elementos
-        if 4000 > area > 1000:
-            qtd_pecas += 1
-            (x, y, w, h) = cv2.boundingRect(cnt)
-
-            cada_peca(roi, x, y, w, h)
-
-            cv2.rectangle(roi, (x, y), (x + w , y + h), (0, 0, 255), 2)
-            cv2.putText(roi, str(area), (x+(w//5), y+(h//2)), 1, 1, ((0, 255, 0)))
-    
+    def identificar_peca(self, frame, x, y, w, h):
+        try:
+            inicio_coluna = x 
+            fim_coluna = x + w 
             
+            inicio_linha = y
+            fim_linha = y + h 
+
+            peca = frame[inicio_linha : fim_linha, inicio_coluna : fim_coluna]
+            self.exibir_resultado('Peça', peca)
+
+            self.comparador_imagens(peca)
+
+            # cv2.imwrite(filename=f'pecas/a.jpg', img=peca)
+        except:
+            pass
+
     
-    if qtd_pecas != pecas_anterior:
-        print(qtd_pecas)
-        pecas_anterior = qtd_pecas
+    def iniciar(self):
+        while True:
+            self.qtd_pecas = 0
+
+            ret, frame = cap.read()
+
+            # Extraindo região importantes
+            self.roi = frame[10:600, 10:600]
+
+            self.mask = cv2.cvtColor(self.roi, cv2.COLOR_BGR2GRAY)
+            self._, self.threshold = cv2.threshold(self.mask, 40, 255, cv2.THRESH_BINARY_INV)
+            # self._, self.threshold = cv2.threshold(self.mask, 100, 255, cv2.THRESH_BINARY)
+            # _, threshold = cv2.threshold(mask, 255, 40, cv2.THRESH_BINARY)
+            
+            self.contours, self._ = cv2.findContours(self.threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            for cnt in self.contours:
+                self.area = cv2.contourArea(cnt)
+
+                # Calculando area e removendo elementos
+                if 4000 > self.area > 1000:
+                    self.qtd_pecas += 1
+                    (x, y, w, h) = cv2.boundingRect(cnt)
+
+                    self.identificar_peca(self.roi, x, y, w, h)
+
+                    cv2.rectangle(self.roi, (x, y), (x + w , y + h), (0, 0, 255), 2)
+                    cv2.putText(self.roi, str(self.area), (x+(w//5), y+(h//2)), 1, 1, ((0, 255, 0)))
+                    
+            
+            if self.qtd_pecas != self.pecas_anterior:
+                print(self.qtd_pecas)
+                self.pecas_anterior = self.qtd_pecas
+
+            # self.exibir_resultado()
+            # self.exibir_resultado('Frame', frame)
+            # self.exibir_resultado('Mask', self.mask)
+            self.exibir_resultado('roi', self.roi)
+            self.exibir_resultado('Mascara', self.threshold)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
 
-    # cv2.imshow('Frame', frame)
-    cv2.imshow('Mask', mask)
-    cv2.imshow('roi', roi)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
+quebra_cabeca = QuebraCabeca()
+cap = quebra_cabeca.cap
+
+quebra_cabeca.iniciar()
+
 cap.release()
 cv2.destroyAllWindows()
